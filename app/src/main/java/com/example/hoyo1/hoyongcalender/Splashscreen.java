59caa2c41eb65a61b8e95375868ddef319b34c4f
hoyo1744 <hoyo1744@naver.com>
2019-05-18 13:16:09 +0900

    20190518 : 스플래쉬 수정완

--- app/src/main/java/com/example/hoyo1/hoyongcalender/Splashscreen.java
+++ app/src/main/java/com/example/hoyo1/hoyongcalender/Splashscreen.java
@@ -59,32 +59,6 @@ public class Splashscreen extends Activity {
                 }
             }
         }, 1300);
-
-
-        /*
-        splashTread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    int waited = 0;
-                    // Splash screen pause time
-                    while (waited <1300) {
-                        sleep(100);
-                        waited += 100;
-                    }
-                    Intent intent = new Intent(Splashscreen.this,
-                            MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(intent);
-                    Splashscreen.this.finish();
-                } catch (InterruptedException e) {
-                    // do nothing
-                } finally {
-                    Splashscreen.this.finish();
-                }
-            }
-        };
-        splashTread.start();
-        */
+        
     }
 }
